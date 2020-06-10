1dd1fd6b09c9631c92884848681a488b29b5700e
Sanchez-Garcia <66310487+Sanchez-Garcia@users.noreply.github.com>
2020-06-10 23:20:28 +0200

    Add files via upload

--- Bolsa_Valores.R
+++ Bolsa_Valores.R
@@ -1,6 +1,6 @@
 #### PASOS PREVIOS####
 #Instalacion de paquetes y librerias necesarias.
-install.packages('bit64')
+# install.packages('bit64')
 install.packages("shiny")
 if(!require("tidyverse")) {
   install.packages("tidyverse", repos="https://cloud.r-project.org/",
@@ -9,6 +9,7 @@ if(!require("tidyverse")) {
 }
 install.packages("gridExtra")
 # install.packages("plotly")
+install.packages("dplyr")
 
 #Llamada a librerias
 library(stringr)
@@ -18,8 +19,9 @@ library(shinydashboard)
 library(dplyr)
 library(gridExtra)
 library(plotly)
-# library(quantmod)
-# library(TTR)
+library(quantmod)
+library(TTR)
+library(dygraphs)
 
 #### LECTURA DE DATOS EXTERNOS ####
 filename_stocks <- list.files("C:/Users/Usuario/Desktop/P2/Stocks", pattern="*.us.txt", full.names=TRUE)
@@ -27,13 +29,21 @@ stock_ticker <- str_match(filename_stocks, "C:/Users/Usuario/Desktop/P2/Stocks/(
 stock_ls <- lapply(filename_stocks, fread)
 names(stock_ls) <- stock_ticker
 stock_dt <- rbindlist(stock_ls, idcol = "Stock_Ticker")
+
 stock_dt[,2] <- as.Date(stock_dt$Date, "%Y-%m-%d")
 stock_dt$Volume <- as.double(stock_dt$Volume)
 stock_dt <- stock_dt[,-8]
+remove(stock_ls)
 
-#### A�ADIMOS ALGUNOS DATOS IMPORTANTES ####
-stock_dt$Color <- stock_dt$Close - stock_dt$Open
-stock_dt$TP <- (stock_dt$High + stock_dt$Low + stock_dt$Close)/3
+# Indicator <- c("ROR", "BB Banwidth")
+# Value <- c(NA, NA)
+# Indicators_df <- data.frame(Indicator, Value)
+# 
+# #### A?ADIMOS ALGUNOS DATOS IMPORTANTES ####
+# stock_dt$chg <- ifelse(stock_dt$Close - stock_dt$Open > 0, "up", "dn")
+# Graph_width <- 0.85
+
+# stock_dt$TP <- (stock_dt$High + stock_dt$Low + stock_dt$Close)/3
 
 # filename_ETF <- list.files("C:/Users/Usuario/Desktop/P2/ETFs", pattern="*.us.txt", full.names=TRUE)
 # ETF_ticker <- str_match(filename_ETF, "C:/Users/Usuario/Desktop/P2/ETFs/(.*?).us.txt")[,2]
@@ -41,101 +51,111 @@ stock_dt$TP <- (stock_dt$High + stock_dt$Low + stock_dt$Close)/3
 # names(etf_ls) <- ETF_ticker
 # etf_dt <- rbindlist(etf_ls, idcol = "ETF_Ticker")
 
-
-# head(etf_dt)
-
-#....#
-# getSymbols("AAPL")
-# chartSeries(AAPL,subset="last 3 months")
-
-# head(AAPL)
-#....#
-
 #### CREACION DE SHINY APP####
-# Define UI for app that draws a histogram ----
-
 ui <- dashboardPage(
                      dashboardHeader(title = "Stock Analysis"),
                      dashboardSidebar(disable = TRUE),
                      dashboardBody(
   fluidRow(
     box(
-      title = "Enter Stock Code", width = 4, solidHeader = TRUE, status = "primary",
+      title = "Enter Stock Code", width = 2, solidHeader = TRUE, status = "primary",
       selectInput("StockName", "Stock", stock_ticker, width = "100%"),
-      dateRangeInput("DateRange", label= "Date Range:", start ="2005-01-01", end = "2015-01-01")
+      dateRangeInput("DateRange", label= "Date Range:", start ="2005-01-01", end = "2015-01-01"),
+      actionButton("Start", "Go"),
+    ),
+    box(
+      title = "Graph Adjustment", width = 10, solidHeader = TRUE, status = "primary",
+      numericInput("Periods", "Number of Periods (in days): ", 20, min = 1, max = 100),
+      radioButtons(inputId = "Options", label = "Graph Type:",
+                   choices = c("Close with Volume" = "Line_Volume",
+                               "Candle Chart" = "Candle",
+                               "Log Returns Normal" = "LogReturns",
+                               "Monte Carlo", "Monte"), 
+                   selected = "Close with Volume")
+      # Poner checkBoxes y demás para customizar el gráfico.
+    )
+    
   ),
-    valueBoxOutput("value1"),
-    valueBoxOutput("value2"),
-    valueBoxOutput("value3"),
-    valueBoxOutput("value4")
-  ),
-  box(
-    title = "Charts", width = "100%", solidHeader = TRUE, status = "primary",
-    plotOutput(outputId = "History", width ="100%")
+  fluidRow(
+    box(
+      title = "Indicators", width = 2, solidHeader = TRUE, status = "primary",
+      tableOutput(output = "Indicators")
+    ),
+    box(
+      title = "Charts", solidHeader = TRUE, status = "primary", width = 10,
+      dygraphOutput(outputId = "History", width ="100%"),
+    )
   )
   )
 )
 
 #### SERVIDOR APP ####
-#Define server logic required to draw a histogram ----
-server <- shinyServer(function(input, output, session) {
+server <- shinyServer (function(input, output, session) ({
   filterDat_dt <- reactive({
-    stock_dt %>% filter((stock_dt$Stock_Ticker == as.character(input$StockName)) &
+    stock_dt[,-1] %>% filter((stock_dt$Stock_Ticker == as.character(input$StockName)) &
                           (stock_dt$Date >= min(input$DateRange) & stock_dt$Date <= max(input$DateRange)))
+  })
+  
+  filterXTS <- reactive({xts(x = filterDat_dt()[,-1], order.by = filterDat_dt()$Date)}) #Se pasa a XTS.
+  bbData <- reactive({BBands(filterXTS()[,4], sd=2.0, n=input$Periods, maType=SMA)}) #Calculo de la Bollinger Bands
+  rsiData <- reactive({RSI(filterXTS()[,4], n=14)}) #Calculo del RSI
+  
+  allData <- reactive({cbind(filterXTS(),bbData(),rsiData())}) #Se quiere juntar todo en un XTS.
+  
+  ROR <- reactive({(filterXTS()$Close[length(filterXTS()$Close)]-filterXTS()$Close[1])/filterXTS()$Close[1]}) #Claculo retorno periodo.
+  BBBW <- reactive({bbData()$up - bbData()$dn}) #Calculo de indicador de riesgo BBBW.
+  
+  # Calcular retornos y ver si tienen una funcionalidad distribucion normal.
+  # Candle <- reactive({allData()[,c(1:4,6:8)]})
+  # dailyROR <- reactive({dailyReturn(Candle()$Close, type = "log")})
+  
+  drawChart <- eventReactive(input$Start,{
+    # if(input$Options == "Line_Volume"){
+    #   dataset <- reactive({allData()[,c(1:5)]})
+    #   output$History <- renderDygraph({
+    #     dygraph(allData()[,4])
+    #   })
+    # }else 
+      if(input$Options == "Candle"){
+      Candle <- reactive({allData()[,c(1:4,6:8)]})
+      output$History <- renderDygraph({
+        p1 <- dygraph(Candle(), main = paste("Stock: ", toupper(input$StockName))) %>% dyCandlestick() %>%
+          dySeries("dn", strokeWidth = 2, strokePattern = "dashed", color = "darkgreen") %>%
+          dySeries("up", strokeWidth = 2, strokePattern = "dashed", color = "darkgreen") %>%
+          dySeries("mavg", strokeWidth = 1.5, color = "red") %>%
+          dyRangeSelector()
+      })
+    }
     })
-  # filterDat_dt$MB <- SMA(filterDat_dt$TP, n=20)
-  # filterDat_dt$UB <- filterDat_dt$MB + 2 * sd(filterDat_dt$TP)
-  # filterDat_dt$LB <- filterDat_dt$MB - 2 * sd(filterDat_dt$TP)
-  
-  output$value1 <- renderValueBox({
-    valueBox(subtitle = "BB BanWidth",
-      mean(4*sd(filterDat_dt$TP)),
-      icon = icon("risk"),
-      color = "blue"
-    )
-  })
-  output$value2 <- renderValueBox({
-    valueBox(subtitle = "KPI 2",
-      sample(1:10, 1),
-      icon = icon("server"),
-      color = "orange"
-    )
-  })
-  output$value3 <- renderValueBox({
-    valueBox(subtitle = "KPI 3",
-      sample(1:10, 1),
-      icon = icon("cookie"),
-      color  = "red"
-    )
-  })
-  output$value4 <- renderValueBox({
-    valueBox(subtitle = "KPI 4",
-             sample(1:10, 1),
-             icon = icon("menu-hamburger", lib = "glyphicon"),
-             color = "green"
-    )
-  })
-  
-  output$History <- renderPlot({
-    f1 <- ggplot(filterDat_dt(), aes(Date,Close)) + scale_colour_identity() + geom_area(color = "black",
-                                                                                   fill = "light grey" ) +
-      theme_minimal()
+     
 
-    f2 <- ggplot(filterDat_dt()) +
-      geom_bar(stat="identity", aes(Date, Volume, fill = ifelse(as.numeric(Color) < 0, "#CA0020", "#4DAC26")),
-               show.legend = FALSE) +
-      theme_minimal()
-    f <- grid.arrange(f1, f2, ncol = 1, heights = c(3, 1.5))
-    })
+  # Value <- reactive({data.frame(cbind("ROR",ROR()))})
+  # Indicators_df()[1,2] <- reactive({ROR()})
+  # Indicators_df()[2,2] <- reactive({BBBW()})
+  
+  output$Indicators <- renderTable(
+   ROR()
+  )
+  #Condicionales para agrupar datos de distintas formas.
+ 
+  # output$LogReturns <- renderPlot({
+  #   dailyROR() %>%
+  #     ggplot(aes(x = daily.returns)) +
+  #     geom_histogram(bins = 100) +
+  #     geom_density() +
+  #     title("Normality of Log Returns") +
+  #     geom_rug(alpha = 0.5)
+  # 
+  # })
+  # output$History <- renderDygraph({
+  #   p1 <- dygraph(Candle(), main = paste("Stock: ", toupper(input$StockName))) %>% dyCandlestick() %>%
+  #     dySeries("dn", strokeWidth = 2, strokePattern = "dashed", color = "darkgreen") %>%
+  #     dySeries("up", strokeWidth = 2, strokePattern = "dashed", color = "darkgreen") %>%
+  #     dySeries("mavg", strokeWidth = 1.5, color = "red") %>%
+  #     dyRangeSelector()
+  # })
 })
-
+)
   #### RUN APP ####
 shinyApp(ui = ui, server = server)
 
-output$value3 <- renderValueBox({
-  valueBox(
-    formatC(prof.prod$value, format="d", big.mark=',')
-    ,paste('Top Product:',prof.prod$Product)
-    ,icon = icon("menu-hamburger",lib='glyphicon')
-    ,color = "yellow")   
-})
---
+++ NoAppCode.R
@@ -0,0 +1,119 @@
+filename_stocks <- list.files("C:/Users/Usuario/Desktop/P2/Stocks", pattern="*.us.txt", full.names=TRUE)
+stock_ticker <- str_match(filename_stocks, "C:/Users/Usuario/Desktop/P2/Stocks/(.*?).us.txt")[,2]
+stock_ls <- lapply(filename_stocks, fread)
+names(stock_ls) <- stock_ticker
+stock_dt <- rbindlist(stock_ls, idcol = "Stock_Ticker")
+
+stock_dt[,2] <- as.Date(stock_dt$Date, "%Y-%m-%d")
+stock_dt$Volume <- as.double(stock_dt$Volume)
+stock_dt <- stock_dt[,-8]
+
+aa <- "amzn"
+
+Date1 <- as.Date("2007-01-01")
+Date2 <- as.Date("2016-10-23")
+
+AMZNData <- stock_dt[,-1] %>% filter((stock_dt$Stock_Ticker == aa) &
+                                       (stock_dt$Date >= Date1 & stock_dt$Date <= Date2))
+
+AMZNxts <- xts(x = AMZNData[,-1], order.by = AMZNData$Date)
+
+bbData <- BBands(AMZNxts[,4], sd=2.0, n=20, maType=SMA) #Calculo de la Bollinger Bands
+rsiData <- RSI(AMZNxts[,4], n=14) #Calculo del RSI
+allData <- cbind(AMZNxts,bbData,rsiData) #Se quiere juntar todo en un XTS.
+names(allData)
+Candle <- allData[,c(1:4,6:8)]
+
+p1 <- dygraph(Candle, main = paste("Stock: ", toupper(aa))) %>% dyCandlestick() %>%
+  dySeries("dn", strokeWidth = 2, strokePattern = "dashed", color = "darkgreen") %>%
+  dySeries("up", strokeWidth = 2, strokePattern = "dashed", color = "darkgreen") %>%
+  dySeries("mavg", strokeWidth = 1.5, color = "red") 
+p1
+
+dailyROR <- dailyReturn(Candle$Close, type = "log")
+names(dailyROR) <- "MA.Log.Returns"
+# Plot the log-returns    
+dailyROR %>%    
+  ggplot(aes(x = MA.Log.Returns)) + 
+  geom_histogram(bins = 100) + 
+  geom_density() +
+  geom_rug(alpha = 0.5)
+
+probs <- c(.005, .025, .25, .5, .75, .975, .995)
+dist_log_returns <- dailyROR %>% 
+  quantile(probs = probs, na.rm = TRUE)
+
+mean_log_returns <- mean(dailyROR, na.rm = TRUE)
+sd_log_returns <- sd(dailyROR, na.rm = TRUE)
+summary1<- data.frame(c("mean","sd"), c(mean_log_returns,sd_log_returns))
+summary1
+
+N     <- 252 # Number of Stock Price Simulations
+M     <- 250  # Number of Monte Carlo Simulations   
+mu    <- mean_log_returns
+sigma <- sd_log_returns
+day <- 1:N
+price_init <- Candle$Close[[nrow(Candle$Close)]]
+# Simulate prices
+set.seed(123)
+monte_carlo_mat <- matrix(nrow = N, ncol = M)
+for (j in 1:M) {
+  monte_carlo_mat[[1, j]] <- price_init
+  for(i in 2:N) {
+    monte_carlo_mat[[i, j]] <- monte_carlo_mat[[i - 1, j]] * exp(rnorm(1, mu, sigma))
+  }
+}
+# Format and organize data frame
+price_sim <- cbind(day, monte_carlo_mat) %>%
+  as_tibble() 
+nm <- str_c("Sim.", seq(1, M))
+nm <- c("Day", nm)
+names(price_sim) <- nm
+price_sim <- price_sim %>%
+  gather(key = "Simulation", value = "Stock.Price", -(Day))
+# Visualize simulation
+price_sim %>%
+  ggplot(aes(x = Day, y = Stock.Price, Group = Simulation)) + 
+  geom_line(alpha = 0.1) +
+  ggtitle(str_c("MA: ", M, 
+                " Monte Carlo Simulations for Prices Over ", N, 
+                " Trading Days"))
+
+# 
+# What can you say about the relationship between the standard deviation and mean of the log returns?
+# Does there appear to be one? 
+# As volatility (standard deviation of returns) increases, what tends to happen to growth (mean increase of returns)?
+
+AMZNData <- stock_dt[,-1] %>% filter((stock_dt$Stock_Ticker == "amzn") &
+                                       (stock_dt$Date >= Date1 & stock_dt$Date <= Date2))
+names(AMZNData)[names(AMZNData) == "Close"] <- "AMZN"
+MSFTData <- stock_dt[,-1] %>% filter((stock_dt$Stock_Ticker == "ndaq") &
+                                       (stock_dt$Date >= Date1 & stock_dt$Date <= Date2))
+names(MSFTData)[names(MSFTData) == "Close"] <- "ndaq"
+AAPLData <- stock_dt[,-1] %>% filter((stock_dt$Stock_Ticker == "aapl") &
+                                       (stock_dt$Date >= Date1 & stock_dt$Date <= Date2))
+names(AAPLData)[names(AAPLData) == "Close"] <- "AAPL"
+NFLXData <- stock_dt[,-1] %>% filter((stock_dt$Stock_Ticker == "nflx") &
+                                       (stock_dt$Date >= Date1 & stock_dt$Date <= Date2))
+names(NFLXData)[names(NFLXData) == "Close"] <- "NFLX"
+XOMData <- stock_dt[,-1] %>% filter((stock_dt$Stock_Ticker == "xom") &
+                                      (stock_dt$Date >= Date1 & stock_dt$Date <= Date2))
+names(XOMData)[names(XOMData) == "Close"] <- "XOM"
+
+data1 <- data.frame(AMZNData$Date,AMZNData$AMZN.Close, MSFTData$Close, AAPLData$Close)
+CorStocks <- data.frame(MSFTData$Date,AMZNData$AMZN, MSFTData$MSFT, AAPLData$AAPL, NFLXData$NFLX, XOMData$XOM)
+names(CorStocks)<- c("Date","AMZN","MSFT","AAPL","NFLX","XOM")
+
+correlation <- CorStocks %>% select(-Date) %>% cor()
+
+correlation %>%  corrplot(order   = "hclust", addrect = 11)
+
+f1 <- ggplot(AAPLData, aes(Date,Close)) + scale_colour_identity() + geom_line(color = "blue") +
+  theme_minimal()
+f1
+
+f2 <- ggplot(AAPLData) +
+  geom_bar(stat="identity", aes(Date, Volume, fill = ifelse(as.numeric(Color) < 0, "red", "green")),
+           show.legend = FALSE) +
+  theme_minimal()
+f <- grid.arrange(f1, f2, ncol = 1, heights = c(3, 1.5))
